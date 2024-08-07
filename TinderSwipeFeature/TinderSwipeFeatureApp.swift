36a83f0f1f9da60787c30f25bd0f25f40ea7fd98
Ganpat Jangir <ganpatlaljangid7688@gmail.com>
2024-08-07 12:27:38 +0530

    Initial Commit

---
+++ TinderSwipeFeature/TinderSwipeFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderSwipeFeatureApp.swift
+//  TinderSwipeFeature
+//
+//  Created by Ganpat Jangir on 07/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TinderSwipeFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
